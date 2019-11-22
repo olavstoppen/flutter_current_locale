317b297a25cf014d657ef7a2a352c049c54eee02
MartinPykPettersen <martin.pettersen@olavstoppen.no>
2019-11-22 22:23:33 +0100

    Get countryId from SIM. Fallback to locale country

--- android/src/main/kotlin/no/olavstoppen/flutter_current_locale/FlutterCurrentLocalePlugin.kt
+++ android/src/main/kotlin/no/olavstoppen/flutter_current_locale/FlutterCurrentLocalePlugin.kt
@@ -1,6 +1,7 @@
 package no.olavstoppen.flutter_current_locale
 
 import android.content.Context
+import android.telephony.TelephonyManager
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -10,6 +11,7 @@ import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class FlutterCurrentLocalePlugin(private val context: Context) : MethodCallHandler
 {
+
   companion object
   {
     @JvmStatic
@@ -47,8 +49,38 @@ class FlutterCurrentLocalePlugin(private val context: Context) : MethodCallHandl
   private fun getCurrentCountryCode(): String?
   {
     // TODO: Get the country code from the phone (dont base it on the Locale as thats not going to be accurate)
-    return null
+    val manager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+    if (manager != null)
+    {
+      val countryId = manager.simCountryIso
+      if (countryId != null && countryId.isNotEmpty())
+      {
+        return countryId
+      }
+    }
+    return fallback()
   }
+
+  private fun fallback(): String?
+  {
+    val configuration = context.resources.configuration
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N)
+    {
+      val list = configuration.locales
+      if (list.size() > 0)
+      {
+        val locale = list.get(0)
+        return locale.country
+      }
+      return null
+    }
+    else
+    {
+      @Suppress("DEPRECATION") val current = context.resources.configuration.locale
+      return current.country
+    }
+  }
+
 
   private fun getCurrentLanguage(): String?
   {
