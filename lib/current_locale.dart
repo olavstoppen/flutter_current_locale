72a266117e97d1593a3f5881cc91feb8e0e510e6
MartinPykPettersen <martin.pettersen@olavstoppen.no>
2021-02-27 22:08:02 +0100

    Don't check in generated files

--- lib/current_locale.dart
+++ lib/current_locale.dart
@@ -3,25 +3,99 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'dart:html' as html;
 
 class CurrentLocale
 {
   static const String kChannelName = "plugins.olavstoppen.no/current_locale";
 
-  bool get isPlatformSupported => Platform.isIOS || Platform.isAndroid;
+  bool get isPlatformSupported
+  {
+    if(kIsWeb) return true;
+    return Platform.isIOS || Platform.isAndroid;
+  }
 
   Future<String> getCurrentLanguage() async
   {
+    if(kIsWeb)
+    {
+      var language = html.window.navigator.language;
+      if(language == null) return "nb";
+      switch(language)
+      {
+        case "nb-NO":
+        case "nb":
+        case "no":
+        case "nn": return "nb";
+        case "en-US":
+        case "en": return "en";
+      }
+      return "nb";
+    }
+
     return _invokeMethod("getCurrentLanguage");
   }
 
   Future<String> getCurrentCountryCode() async
   {
+    if(kIsWeb)
+    {
+      var language = html.window.navigator.language;
+      if(language == null) return "NO";
+      switch(language)
+      {
+        case "nb-NO":
+        case "nb":
+        case "no":
+        case "nn": return "NO";
+        case "en-US":
+        case "en": return "US";
+      }
+      return "NO";
+    }
+
     return _invokeMethod("getCurrentCountryCode");
+  }
+
+  CurrentLocaleResult get noCurrentLocalResult
+  {
+    return CurrentLocaleResult(
+        identifier: "nb_NO",
+        decimals: ",",
+        language: CurrentLocaleInfo(phone: "nb",locale: "no"),
+        country: CurrentCountryInfo(phone: null, locale: "NO", region: "NO")
+    );
+  }
+
+  CurrentLocaleResult get enCurrentLocaleResult
+  {
+    return CurrentLocaleResult(
+        identifier: "en_US",
+        decimals: ",",
+        language: CurrentLocaleInfo(phone: "en",locale: "en"),
+        country: CurrentCountryInfo(phone: null, locale: "US", region: "US")
+    );
   }
 
   Future<CurrentLocaleResult> getCurrentLocale() async
   {
+    if(kIsWeb)
+    {
+      var language = html.window.navigator.language;
+      if(language == null) return Future.value(noCurrentLocalResult);
+      switch(language)
+      {
+        case "nb-NO":
+        case "nb":
+        case "no":
+        case "nn": return Future.value(noCurrentLocalResult);
+        case "en-US":
+        case "en": return Future.value(enCurrentLocaleResult);
+      }
+      return Future.value(noCurrentLocalResult);
+    }
+
+
     var supported = isPlatformSupported;
     if (!supported)
       return Future.value(null);
@@ -34,10 +108,10 @@ class CurrentLocale
       {
         String getString(String key)
         {
-           String identifier;
-            if (result.containsKey(key))
-              identifier = result[key];
-            return identifier;
+          String identifier;
+          if (result.containsKey(key))
+            identifier = result[key];
+          return identifier;
         }
 
         CurrentLocaleInfo getLocale(String key)
@@ -64,11 +138,11 @@ class CurrentLocale
           try
           {
             var d = Map<String, String>.from(result[key]);
-            String region;            
+            String region;
             if (d.containsKey("region"))
-              region = d["region"];            
+              region = d["region"];
             return CurrentCountryInfo(locale:info.locale,phone:info.phone,region:region);
-          } 
+          }
           catch (e)
           {
             return CurrentCountryInfo(locale:info.locale,phone:info.phone);
@@ -132,6 +206,6 @@ class CurrentLocaleInfo
 class CurrentCountryInfo extends CurrentLocaleInfo
 {
   final String region;
-  
+
   CurrentCountryInfo({String phone,String locale,this.region}) : super(phone:phone,locale:locale);
 }
